d81f279f654ee0ed647f278f4234b26bcd996fe6
Aiswaryasuresh95 <aiswaryapoozhikkala@gmail.com>
2020-09-20 19:58:22 +0530

    worked on props

--- src/components/Listitem.component.jsx
+++ src/components/Listitem.component.jsx
@@ -4,25 +4,25 @@ import './list.styles.css';
 import { Trash } from 'react-feather';
 
 
-function ListItems(props){
-    const item = props.items;
+function ListItems({items,deleteItem,completed,CheckComplete}){
+    const item = items;
     // const date = moment().format("DD MM YYYY");
     
     const listitems = item.map( list =>{
           
         return(
-            <div key={list.key} className={props.completed?'list completed':'list'} >
+            <div key={list.key} className={completed?'list completed':'list'} >
                
-                  {props.completed? <input type="checkbox" Checked={`${props.completed}`} onClick={(e)=>props.CheckComplete(list.key,e.target.value)}/>
+                  {completed? <input type="checkbox" Checked={`${completed}`} onClick={(e)=>CheckComplete(list.key,e.target.value)}/>
                   :
-                 <input type="checkbox"  onClick={()=>props.CheckComplete(list.key)}/>}
+                 <input type="checkbox"  onClick={()=>CheckComplete(list.key)}/>}
 
        
 
                     <input type="text" id={item.key}  defaultValue={list.text} /> 
                 <span>
                     <button className="trash">
-                          <Trash  size="13" color="#ccb9bc" onClick={()=>props.deleteItem(list.key)}/>
+                          <Trash  size="13" color="#ccb9bc" onClick={()=>deleteItem(list.key)}/>
                     </button>
                  </span>
                 
